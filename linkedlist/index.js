c6c447d8407ab1d36e2f526c0ad6c12247040692
Nick <lamothe.dev@gmail.com>
2022-03-25 16:28:42 -0400

    added clear(), removeFirst(), removeLast() for linked list

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -52,23 +52,73 @@ class LinkedList {
          node = node.next;
       }
    }
-}
 
-class LinkedListOld {
-   constructor() {
+   clear() {
       this.head = null;
    }
 
-   // original implementation (flawed)
-   insertFirst(data) {
-      if (this.head == null) {
-         const n = new Node(data);
-         this.head = n;
+   removeFirst() {
+      if (!this.head) {
+         return;
       }
-      else {
-         const n = new Node(data, this.head);
-         this.head = n;
+
+      let headNode = this.head;
+      let secondNode = headNode.next;
+
+      this.head = secondNode;
+
+      // cleaner solution: this.head = this.head.next (one line of code vs three)
+   }
+
+   // 1) if empty, return
+   // 2) if this.head.next doesn't exist, then there's only one node, which we set to null
+   // 3) if we have more than two nodes, get the last node, and iterate until node.next is equal to the last node,
+   //    then simply set node.next to null (which rids of the last node)
+   removeLast() {
+      if (!this.head) {
+         return;
       }
+
+      if (!this.head.next) {
+         this.head = null;
+         return;
+      }
+
+      let lastNode = this.getLast();
+      let node = this.head;
+      
+      while (node) {
+         if (node.next === lastNode) {
+            node.next = null;
+            break;
+         }
+
+         node = node.next;
+      }
+   }
+   
+   // my solution for removeLast() is quite distinct from Grider's solution below:
+   // he iterates through the list while there exists a node.next, using 'node' and 'previous' pointers for nodes;
+   // once arriving at the end, he then sets previous.next to null -- perfectly good solution
+   removeLastAlt() {
+      if (!this.head) {
+         return;
+      }
+
+      if (!this.head.next) {
+         this.head = null;
+         return;
+      }
+
+      let previous = this.head;
+      let node = this.head.next;
+
+      while (node.next) {
+         previous = node;
+         node = node.next;
+      }
+
+      previous.next = null;
    }
 }
 
