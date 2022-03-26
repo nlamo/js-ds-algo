00c8e57be2d7a43287b25c1981d478450ca4ee18
Nick <lamothe.dev@gmail.com>
2022-03-25 20:12:06 -0400

    insertLast(), getAt(), removeAt() methods for linked list

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -7,7 +7,7 @@ class Node {
    constructor(data, Node = null) {
       this.data = data;
       this.next = Node;
-   }
+   } 
 }
 
 // Linked list with various methods
@@ -17,7 +17,7 @@ class LinkedList {
    }
 
    insertFirst(data) {
-      const node = new Node(data, this.head);
+      let node = new Node(data, this.head);
       this.head = node;
    }
 
@@ -67,7 +67,7 @@ class LinkedList {
 
       this.head = secondNode;
 
-      // cleaner solution: this.head = this.head.next (one line of code vs three)
+      // cleaner solution from Grider: this.head = this.head.next (one line of code vs three)
    }
 
    // 1) if empty, return
@@ -120,20 +120,105 @@ class LinkedList {
 
       previous.next = null;
    }
+
+   insertLast(data) {
+      let node = new Node(data);
+      let lastNode = this.getLast();
+
+      // if the list is empty, then we can just insert first node (which will also be the last)
+      if (!this.head) {
+         this.insertFirst(data);
+         return;
+      }
+
+      lastNode.next = node;
+   }
+
+   // Grider's implementation
+   insertLastAlt(data) {
+      let last = this.getLast();
+
+      if (last) {
+         // There are some existing nodes in our chain
+         last.next = new Node(data);
+      }
+      else {
+         // The chain is empty!
+         this.head = new Node(data);
+      }
+   }
+
+   getAt(index) {
+      let count = 0;
+      let node = this.head;
+
+      while (node) {
+         if (count == index) {
+            return node;
+         }
+
+         node = node.next;
+         count++;
+      }
+
+      return null;
+   }
+   
+   // 1) if there's no head, the list is empty, simply return
+   // 2) if there's a head but no second node, and if index is 0, then we set the head to null and return;
+   //    otherwise we simply return
+   // 3) if there's a head and a second node, and if index is 0, then we set head to head.next, but we 
+   //    do not return, as based on the first conditional, execution may need to proceed
+   // 4) the main algorithm: we have the main node (this.head), we have the node to be removed (this.getAt(index));
+   //    we iterate over each node, and if node.next is equal to nodeToRemove, then we set node.next to be 
+   //    nodeToRemove.next
+   //       * the 'next' reference of the node *before* nodeToRemove is changed to the node *after* nodeToRemove
+   removeAt(index) {
+      if (!this.head) {
+         return;
+      }
+
+      if (this.head && !this.head.next) {
+         if (index == 0) {
+            this.head = null;
+            return;
+         }
+         else {
+            return;
+         }
+      }
+
+      if (this.head && this.head.next) {
+         if (index == 0) {
+            this.head = this.head.next;
+         }
+      }
+
+      let node = this.head;
+      let nodeToRemove = this.getAt(index);
+
+      while (node) {      
+         if (node.next === nodeToRemove) {
+            node.next = nodeToRemove.next;
+         }
+
+         node = node.next;
+      }
+   }
 }
 
 // ----------------------------------------------------------
 
 // Basic test of the Node itself, its linkability
-const n = new Node('There');
+let n = new Node('There');
 console.log(n.data);
 console.log(n.next);
 
-const n2 = new Node('Hi', n);
+let n2 = new Node('Hi', n);
 console.log(n2.next);
 
 // Creation of the linked list
-const list = new LinkedList();
+let list = new LinkedList();
 console.log(list.head);
 
 module.exports = { Node, LinkedList };
